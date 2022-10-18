d15ac77df3794fff94f0135b22f3fb03ff0797ab
Jarek700 <a70t9zz@mmm.com>
2022-10-18 12:37:36 +0200

    some fun with creating objects

--- scripts/script.js
+++ scripts/script.js
@@ -1,88 +1,29 @@
+//stwórz obiekt który pobiera poprzez prompt imiona i nazwiska osób i zapisuje w tablicy.
 
-function calculator() {
-    const nbr1 = Number(prompt("Podaj pierwszą liczbę:"));
-    const nbr2 = Number(prompt("Podaj drugą liczbę:"));
-    const sign = prompt("Podaj znak (+,-,/,*):")
-    calculate(nbr1, nbr2, sign);
+function Person(firstName, surName) {
+    this.firstName = firstName;
+    this.surName = surName;
 }
 
-function calculate(firstNumber, secondNumber, whatToDo) {
-    validateNumber(firstNumber);
-    validateNumber(secondNumber);
-    validateMathsign(whatToDo);
-    const text = "wynik twojego działania = "
-    var result = "";
-    if (whatToDo === '+') {
-        result = result + sum2args(firstNumber, secondNumber)
+const addListOfPeople = () => {
+    const numberOfPeopleToAdd = prompt("Podaj ile osób chcesz dodać");
+    const arrayOfPeople = [];
+    for (let i = 0; i < numberOfPeopleToAdd; i++) {
+        const personName = prompt("podaj imię osoby nr: " + (i + 1) + " na liście:");
+        const personSurname = prompt("podaj nazwisko osoby nr: " + (i + 1) + " na liście:");
+        arrayOfPeople[i] = new Person(personName, personSurname);
     }
-    if (whatToDo === '-') {
-        result = result + substraction2args(firstNumber, secondNumber)
+    alert(JSON.stringify(arrayOfPeople));
+    console.log(arrayOfPeople)
+}
+//wersja bez zmiennych.
+const addListOfPeople2 = () => {
+    const numberOfPeopleToAdd = prompt("Podaj ile osób chcesz dodać"), arrayOfPeople = [];
+    for (let i = 0; i < numberOfPeopleToAdd; i++) {
+        arrayOfPeople[i] = new Person(
+            prompt("podaj imię osoby nr: " + (i + 1) + " na liście:"),
+            prompt("podaj nazwisko osoby nr: " + (i + 1) + " na liście:"));
     }
-    if (whatToDo === '*') {
-        result = result + multiplication2args(firstNumber, secondNumber)
-    }
-    if (whatToDo === '/') {
-        result = result + devide2args(firstNumber, secondNumber)
-    }
-    alert(text + result);
-}
 
-const sum2args = (arg1, arg2) => {
-    return arg1 + arg2;
-}
-const devide2args = (arg1, arg2) => {
-    return arg1 / arg2;
-}
-const substraction2args = (arg1, arg2) => {
-    return arg1 - arg2;
-}
-const multiplication2args = (arg1, arg2) => {
-    return arg1 * arg2;
-}
-
-function validateNumber(value) {
-    if (typeof value !== 'number' || Number.isNaN(value)) {
-        alert("musisz podać numer aby dokonać obliczeń")
-        return;
-    }
-    return value;
-}
-
-function validateMathsign(sign) {
-    if (!(sign === '+' || sign === '-' || sign === '/' || sign === '*')) {
-        alert("musisz podać prawidłowy znak do obliczeń")
-        return;
-    }
-    return sign;
-}
-
-
-function oddOrNot() {
-    let valueToBeVerified = Number(prompt("podaj liczbe"));
-    const comebackValue = checkNumber(valueToBeVerified);
-    alert(comebackValue);
-}
-
-function checkNumber(value) {
-    if (typeof value !== 'number' || Number.isNaN(value)) {
-        alert("musisz podać prawidłowy numer")
-        return;
-    }
-    if (value % 2 == 0) {
-        return value;
-    }
-    return value * 2;
-}
-
-
-const oddOrNot2 = () => {
-    oddOrNot();
-}
-
-const checkNumber2 = (value) => {
-    checkNumber(value);
-}
-
-const calculator2 = () => {
-    calculator();
+    console.log(arrayOfPeople)
 }
