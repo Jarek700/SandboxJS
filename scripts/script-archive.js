b99c564590d20302deacf1318fe1e471169e91de
Jarek700 <a70t9zz@mmm.com>
2022-10-27 16:00:11 +0200

    start of DOM

--- scripts/script-archive.js
+++ scripts/script-archive.js
@@ -613,3 +613,33 @@ const aRequire = () => {
     })();
 }
 
+const bookByIdasync = () => {
+    //const idOfBookISBN = 9781312173705;
+    const idOfBookISBN = Number(prompt('podaj numer ISBN książki:'));
+
+    (async () => {
+        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${idOfBookISBN}`)
+        const data = await response.json()
+        if (!data.items || data.items.length === 0) {
+            console.log('Book not found!');
+        } else {
+            const { authors, title } = data.items[0].volumeInfo
+            console.log(`${title} by ${authors}`);
+        }
+    })();
+}
+const stywarsAwait = () => {
+    (async () => {
+        const response = await fetch('https://swapi.dev/api/people/3');
+        const data = await response.json();
+        console.log(data.name);
+
+    })();
+
+    async function lukeskywakeru() {
+        const reponse = await fetch('https://swapi.dev/api/people/1')
+        const data = await reponse.json();
+        console.log(data.name)
+    }
+    lukeskywakeru()
+}
